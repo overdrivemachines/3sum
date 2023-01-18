34af25a47326cd2671bd997cdd5fe22bf6d8809a
get.dipen@gmail.com <get.dipen@gmail.com>
2023-01-18 14:21:58 -0800

    Optimized

--- 3sum.cpp
+++ 3sum.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-vector<vector<int>> threeSum(vector<int>& nums) {
+vector<vector<int>> threeSum_slow(vector<int>& nums) {
   vector<vector<int>> result;
   unordered_map<int, vector<pair<int, int>>> umap;
   unordered_map<int, vector<pair<int, int>>>::iterator umapit;
@@ -90,6 +90,83 @@ vector<vector<int>> threeSum(vector<int>& nums) {
 
   return result;
 
+}
+
+// optimized solution from
+// https://leetcode.com/problems/3sum/solutions/1462423/c-both-two-pointers-and-hashmap-approach-explained/
+vector<vector<int>> threeSum(vector<int>& nums) {
+  vector<vector<int>> result;
+  int element1, element2, element3;
+  int low, high; // pointers
+  int sum;
+
+  // if nums vector has less than 3 elements return
+  if (nums.size() < 3)
+    return result;
+
+  // Sort the nums vector
+  sort(nums.begin(), nums.end());
+
+  for (auto num : nums)
+    cout << num << " ";
+  cout << "\n";
+
+  // if there are no negative numbers return
+  if (nums[0] > 0)
+    return result;
+
+  // iterating through the nums vector
+  for (int i = 0; i < nums.size(); i++) {
+    element1 = nums[i];
+
+    // if element1 is +ve no point continuing because we cannot make a sum of 0
+    if (element1 > 0)
+      break;
+
+    if (i > 0) {
+      if (element1 == nums[i - 1]) {
+        // if element1 is equal to previous element, move on to the next iteration
+        continue;
+      }
+    }
+
+    low = i + 1;
+    high = nums.size() - 1;
+    sum = 0;
+    // using the low and high pointer find 2 elements that are the complement of element1
+
+    while (low < high) {
+      // -4, -1, -1, 0, 1, 2
+      element2 = nums[low];
+      element3 = nums[high];
+      sum = element1 + element2 + element3;
+
+      // if sum is too high we need to decrement the high pointer
+      if (sum > 0)
+        high--;
+      else if (sum < 0) // sum is too low, we need to increment the low pointer
+        low++;
+      else {
+        // sum is 0, we have found the triplet
+        result.push_back({element1, element2, element3});
+        // cout << "[" << element1 << "," << element2 << "," << element3 << "] ";
+        // -1, -1,  2
+        // [1] [2] [5]
+        //  i  low high
+        //  nums: -4, -1, -1, 0, 1, 2
+
+        // increament low if nums[low+1] is the same as nums[low]
+        while (low < high && nums[low] == element2)
+          low++;
+
+        // decrement high if nums[high-1] is the same as nums[high]
+        while (low < high && nums[high] == element3)
+          high--;
+      }
+    }
+  }
+
+  return result;
 }
 
 // Print a vector of vector of ints
