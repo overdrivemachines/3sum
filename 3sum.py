10eaa365d3a46a9014564a62a6fe9f582e653ba5
Dipen Chauhan <get.dipen@gmail.com>
2023-07-15 21:21:32 -0700

    found solution

--- 3sum.py
+++ 3sum.py
@@ -1,33 +1,34 @@
 from typing import List
+
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        result = []
+        res = []
         nums.sort()
-        print("Sorted:", nums)
-        for i, num in enumerate(nums):
-            # skip this loop if current num is same as previous num
-            if num != nums[i-1]:
-                # now find the pair that adds up to -num
-                # using 2 pointer (left and right) approach
-                left = i + 1
-                right = len(nums) - 1
-                while (left < right):
-                    # Checking total
-                    print("nums[", i, "]: ", num, ": ", nums[left], " + ", nums[right])
-                    total = (nums[left] + nums[right])
-                    if (total < (num * -1)):
-                        left += 1
-                    elif (total > (num * -1)):
-                        right -= 1
-                    else:
-                        result.append([num, nums[left], nums[right]])
-                        print("Inserted ", [num, nums[left], nums[right]])
-                        left += 1
-                        right = len(nums) - 1
 
-        return result
+        for i, num in enumerate(nums):
+            # check if current number is the same as previous number
+            if i > 0 and num == nums[i - 1]:
+                continue
+
+            # 2 pointers:
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                threeSum = num + nums[l] + nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
+                else:
+                    res.append([num, nums[l], nums[r]])
+                    # Update the pointer
+                    l += 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+        return res
+
 
 s = Solution()
 # nums = [-1,0,1,2,-1,-4]
-nums = [0,0,0,0]
+nums = [0, 0, 0, 0]
 print(s.threeSum(nums))
